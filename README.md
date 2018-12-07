7c74217b8583d059084a22f8c937fefcb3697bf7
Lukas Dachtler <software@lukas-dachtler.de>
2018-12-07 07:55:36 +0100

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,36 @@
+# Simply-Minify
+[![NPM version](https://img.shields.io/npm/v/simply-minify.svg?style=flat)]()
+[![NPM Downloads](https://img.shields.io/npm/dt/simply-minify.svg)]()
+#### Simply-Minify is a simple minifier for the [Node.js](http://nodejs.org/) platform. Simply-Minify finds all CSS and JS files in the given folder and minifies them.
+### Dependencies:
+NPM-Package|Version
+---|---
+**clean-css**|[![NPM version](https://img.shields.io/badge/npm-v4.2.1-blue.svg)](https://www.npmjs.com/package/clean-css)
+**UglifyJS3**|[![NPM version](https://img.shields.io/badge/npm-v3.4.9-blue.svg)](https://www.npmjs.com/package/uglify-js)
+# Install
+```
+npm install simply-minify
+```
+# Use
+```js
+const simplyMinify = require('simply-minify');
+simplyMinify();
+```
+This will minify all files in the `"dist"` folder in the **root directory** of your app.
+## Arguments
+The function `simplyMinify` takes 3 arguments with the following default values:
+
+argument|data type|description|default|example
+---|---|---|---|---
+`dir`|`string`|directory to search for files|`"dist"`|*`"src/js"`*
+`CleanCSS_Options`|`object`|[options for clean-css](https://github.com/jakubpawlowicz/clean-css/blob/master/README.md#constructor-options)|`{}`|*`{level: 0}`*
+`UglifyJS_Options`|`object`|[options for UglifyJS3](https://github.com/mishoo/UglifyJS2#minify-options)|`{}`|*`{mangle: false}`*
+## CLI mode
+***Make sure you have installed the npm-package globally:*** `npm install simply-minify -g`
+You can use simply-minify straight from the command line with:
+```js
+simplyMinify <path>
+```
+> *Options are not supported in CLI mode*
+# License
+Simply-Minify is released under the [MIT License](https://github.com/lukas-dachtler/simply-minify/blob/master/LICENSE.md).
---
+++ index.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const path = require('path');
+const UglifyJS = require('uglify-js');
+const CleanCSS = require('clean-css');
+let CleanCSS_INSTANCE;
+let _UglifyJS_Options;
+const base = path.dirname(require.main.filename);
+
+if (require.main === module) {
+    const args = process.argv.slice(2);
+    if (args.length === 0) {
+        simplyMinify('dist');
+    } else {
+        let p = args[0];
+        if (!path.isAbsolute(p)) {
+            p = path.join(base, p);
+        }
+        if (args.length > 1) {
+            console.log('WARNING: Options are ignored in CLI mode!');
+        }
+        simplyMinify(p);
+    }
+} else {
+    module.exports = simplyMinify;
+}
+
+function simplyMinify(dir = 'dist', CleanCSS_Options = {}, UglifyJS_Options = {}) {
+    CleanCSS_INSTANCE = new CleanCSS(CleanCSS_Options);
+    _UglifyJS_Options = UglifyJS_Options;
+    _UglifyJS_Options['warnings'] = true;
+    let finalDir;
+    if (dir === 'dist' || !path.isAbsolute(dir)) {
+        finalDir = path.join(base, dir);
+    } else {
+        finalDir = dir;
+    }
+    if (fs.existsSync(finalDir)) {
+        findFiles(finalDir);
+    } else {
+        console.log('ERROR: Directory not found!');
+    }
+}
+
+function findFiles(dir) {
+    fs.readdirSync(dir).forEach(function (name) {
+        const item = path.join(dir, name);
+        const stat = fs.statSync(item);
+        if (stat.isFile()) {
+            if (item.match(/\.min\./)) {
+            } else if (item.match(/\.css/)) {
+                compileFile(item, 'css');
+            } else if (item.match(/\.js/)) {
+                compileFile(item, 'js');
+            }
+        } else if (stat.isDirectory()) {
+            findFiles(item);
+        }
+    })
+}
+
+function compileFile(input, type) {
+    const regex = new RegExp('\.' + type + '$', '');
+    const output = input.replace(regex, '.min.' + type);
+    const uncompressed = fs.readFileSync(input, 'utf8');
+    let compressed = null;
+    let result;
+    switch (type) {
+        case 'css':
+            result = CleanCSS_INSTANCE.minify(uncompressed);
+            if (result.warnings.length > 0) {
+                let message = '\n';
+                result.warnings.forEach(function (warning) {
+                    message = message + warning.replace(/(?:\r\n|\r|\n)/g, '').replace(/ +(?= )/g, '') + '\n';
+                });
+                console.log('WARNINGS <' + path.basename(input) + '> : ' + message);
+            }
+            if (result.errors.length > 0) {
+                let message = '';
+                result.errors.forEach(function (error) {
+                    message = message + error.replace(/(?:\r\n|\r|\n)/g, '').replace(/ +(?= )/g, '') + '\n';
+                });
+                console.log('ERROR <' + path.basename(input) + '> : ' + message);
+            } else {
+                compressed = result.styles;
+            }
+            break;
+        case 'js':
+            result = UglifyJS.minify(uncompressed, _UglifyJS_Options);
+            if (result.warnings.length > 0) {
+                let message = '\n';
+                result.warnings.forEach(function (warning) {
+                    message = message + warning.replace(/(?:\r\n|\r|\n)/g, '').replace(/ +(?= )/g, '') + '\n';
+                });
+                console.log('WARNINGS <' + path.basename(input) + '> : ' + message);
+            }
+            if (result.error) {
+                console.log('ERROR <' + path.basename(input) + '> : ' + result.error.message.replace(/(?:\r\n|\r|\n)/g, '').replace(/ +(?= )/g, ''));
+            } else {
+                compressed = result.code;
+            }
+            break;
+        default:
+            return;
+    }
+    if (compressed !== null) {
+        fs.writeFileSync(output, compressed);
+        console.log('Minified: ', path.basename(input), '->', path.basename(output));
+    }
+}
